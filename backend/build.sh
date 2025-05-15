0d07628c74825a102a2bc89c172a3b035a108b72
Aleks <55019798+32zobi12@users.noreply.github.com>
2025-05-16 01:02:23 +0800

    Fix Rneder

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit  # Останавливает скрипт при любой ошибке
 
+# Установка зависимостей
 pip install -r backend/requirements.txt
 
-# (опционально) Сборка статики и миграции
-python backend/manage.py collectstatic --noinput
+# Миграции и сборка статики
 python backend/manage.py migrate
+python backend/manage.py collectstatic --noinput

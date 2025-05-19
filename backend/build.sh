0cd19977adaf9aa0a27f1982843f39b2a220d75c
Aleks <55019798+32zobi12@users.noreply.github.com>
2025-05-19 14:52:10 +0800

    change superuser

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -o errexit  # Останавливает скрипт при любой ошибке
+set -o errexit
 
 # Установка зависимостей
 pip install -r requirements.txt
@@ -7,3 +7,11 @@ pip install -r requirements.txt
 # Миграции и сборка статики
 python manage.py migrate
 python manage.py collectstatic --noinput
+
+# Создание суперпользователя (если не существует)
+echo "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='myd').exists():
+    User.objects.create_superuser('myd', 'admin@example.com', 'jjv2xUquDC4uc2S')
+" | python manage.py shell
